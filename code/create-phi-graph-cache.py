d6abac82c982c6a448ccefc5e2fb94d7c561e74c
David Gengenbach <davidgengenbach@gmx.de>
2017-08-17 22:38:02 +0200

    Added phi cache script

--- code/create-phi-graph-cache.py
+++ code/create-phi-graph-cache.py
@@ -6,7 +6,7 @@ import graph_helper
 import gc
 import pickle
 from transformers.fast_wl_graph_kernel_transformer import FastWLGraphKernelTransformer
-
+import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
@@ -14,12 +14,15 @@ for graph_cache_file in dataset_helper.get_all_cached_graph_datasets():
     phi_graph_cache_file = graph_cache_file.replace('.npy', '.phi.npy')
     print('{} {}'.format('#' * 20, graph_cache_file.split('/')[-1]))
     if '.phi.' in graph_cache_file: continue
-        
+
     if os.path.exists(phi_graph_cache_file):
         print('\tAlready calculated phi: {}'.format(phi_graph_cache_file))
         continue
-
     gc.collect()
+
+    with open(phi_graph_cache_file, 'w') as f:
+        f.write(':)')
+
     try:
         X, Y = dataset_helper.get_dataset_cached(graph_cache_file)
         transformer = FastWLGraphKernelTransformer(h=4, remove_missing_labels=True)
