539e75fb9761743f23f380e94724fcdc92098930
David Gengenbach <davidgengenbach@gmx.de>
2017-08-18 01:09:41 +0200

    Fix

--- code/run-classification.py
+++ code/run-classification.py
@@ -26,9 +26,10 @@ from transformers.preprocessing_transformer import PreProcessingTransformer
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-N_JOBS = 4
+N_JOBS = 1
 
 for dataset_name in dataset_helper.get_all_available_dataset_names():
+    break
     result_file = 'data/results/text_{}.results.npy'.format(dataset_name)
 
     if os.path.exists(result_file): continue
@@ -61,54 +62,54 @@ for dataset_name in dataset_helper.get_all_available_dataset_names():
 for cache_file in dataset_helper.get_all_cached_graph_phi_datasets():
     graph_dataset_cache_file = cache_file.split('/')[-1]
     
-    result_file = 'data/results/{}.results.npy'.format(graph_dataset_cache_file)
+    
     logger.info('{}\tDataset File: {}'.format('#' * 10, graph_dataset_cache_file))
 
-    if os.path.exists(result_file):
-        logger.warning('\tAlready calculated result: {}'.format(result_file))
-        continue
 
     if not os.path.exists(cache_file):
         logger.warning('\tCould not find cachefile: "{}". Skipping.'.format(cache_file))
         continue
     gc.collect()
     
-    X, Y = dataset_helper.get_dataset_cached(cache_file, check_validity = False)
-    #X, Y = np.array(X), np.array(Y)
-    
-    p = Pipeline([
-        #('wl_transformer', FastWLGraphKernelTransformer()),
-        ('phi_picker', PhiPickerTransformer()),
-        ('scaler', None),
-        ('clf', None)
-    ])
+    X_all, Y = dataset_helper.get_dataset_cached(cache_file, check_validity = False)
+    # Hack
 
-    param_grid = dict(
-        #wl_transformer__h=[2],
-        phi_picker__return_iteration=[0, 1, 2, 3, 4],
-        scaler = [None, sklearn.preprocessing.Normalizer(norm="l1", copy = False)],
-        clf = [sklearn.linear_model.PassiveAggressiveClassifier(max_iter = 1000)],
-        clf__max_iter=[1000],
-        clf__tol = [1e-3],
-        clf__class_weight=['balanced']
-    )
-
-    cv = sklearn.model_selection.StratifiedKFold(n_splits = 3, random_state= 42, shuffle= True)
-    gscv = GridSearchCV(
-        estimator = p,
-        param_grid=param_grid,
-        cv=cv,
-        scoring = 'f1_macro',
-        n_jobs=N_JOBS,
-        verbose = 11
-    )
-    if 1 == 1:
-        try:
-            gscv_result = gscv.fit(X, Y)
-        except Exception as e:
-            logger.exception(e)
+    for h in range(len(X_all)):
+        result_file = 'data/results/{}.{}.results.npy'.format(graph_dataset_cache_file, h)
+        if os.path.exists(result_file):
+            logger.warning('\tAlready calculated result: {}'.format(result_file))
             continue
-    else:
-        gscv_result = gscv.fit(X, Y)
-    with open(result_file, 'wb') as f:
-        pickle.dump(gscv_result.cv_results_, f)
+        X = [x.T for x in X_all[h]]
+
+        p = Pipeline([
+            ('scaler', None),
+            ('clf', None)
+        ])
+
+        param_grid = dict(
+            scaler = [None, sklearn.preprocessing.Normalizer(norm="l1", copy = False)],
+            clf = [sklearn.linear_model.PassiveAggressiveClassifier(max_iter = 1000)],
+            clf__max_iter=[1000],
+            clf__tol = [1e-3],
+            clf__class_weight=['balanced']
+        )
+
+        cv = sklearn.model_selection.StratifiedKFold(n_splits = 3, random_state= 42, shuffle= True)
+        gscv = GridSearchCV(
+            estimator = p,
+            param_grid=param_grid,
+            cv=cv,
+            scoring = 'f1_macro',
+            n_jobs=N_JOBS,
+            verbose = 11
+        )
+        if 1 == 1:
+            try:
+                gscv_result = gscv.fit(X, Y)
+            except Exception as e:
+                logger.exception(e)
+                continue
+        else:
+            gscv_result = gscv.fit(X, Y)
+        with open(result_file, 'wb') as f:
+            pickle.dump(gscv_result.cv_results_, f)
--- code/transformers/phi_picker_transformer.py
+++ code/transformers/phi_picker_transformer.py
@@ -1,10 +1,12 @@
 import sklearn
 
+
 class PhiPickerTransformer(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
 
-    def __init__(self, return_iteration = 0):
+    def __init__(self, return_iteration=0, transpose=True):
         assert return_iteration >= -1
         self.return_iteration = return_iteration
+        self.transpose = False
 
     def fit(self, X, y=None, **fit_params):
         return self
@@ -12,4 +14,5 @@ class PhiPickerTransformer(sklearn.base.BaseEstimator, sklearn.base.TransformerM
     def transform(self, X, y=None, **fit_params):
         # X should be a list of phi matrices
         assert self.return_iteration < len(X) or self.return_iteration == -1
-        return X[self.return_iteration].T
+        target = X[self.return_iteration]
+        return target.T if transpose else target
