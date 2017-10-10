2660e99a7019be998f2ac42a17e8bc9a1425c4d7
David Gengenbach <davidgengenbach@gmx.de>
2017-10-10 23:55:41 +0200

    Stats

--- code/transformers/phi_picker_transformer.py
+++ code/transformers/phi_picker_transformer.py
@@ -5,7 +5,11 @@ from scipy import sparse
 class PhiPickerTransformer(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
 
     def __init__(self, return_iteration=0, transpose=False):
+        if return_iteration != 'stacked' and isinstance(return_iteration, str):
+            return_iteration = int(return_iteration)
+
         assert return_iteration == 'stacked' or return_iteration >= -1
+
         self.return_iteration = return_iteration
         self.transpose = transpose
 
