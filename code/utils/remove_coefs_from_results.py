69b004e134771cbe5f440d43f230351deb8c00cf
David Gengenbach <davidgengenbach@gmx.de>
2017-11-07 21:53:23 +0100

    Remove more coefs

--- code/utils/remove_coefs_from_results.py
+++ code/utils/remove_coefs_from_results.py
@@ -7,9 +7,8 @@ import numpy as np
 
 def remove_coefs(clf):
     found = False
-    for x in ['coef_', 'class_log_prior_', 'intercept_', 'feature_log_prob_', 'class_count_', 'feature_count_', 'vocabulary_', 'idf_', 'stop_words_']:
+    for x in ['coef_', 'class_log_prior_', 'intercept_', 'feature_log_prob_', 'class_count_', 'feature_count_', 'vocabulary_', 'idf_', 'stop_words_', '_tfidf']:
         try:
-            #delattr(clf, x)
             setattr(clf, x, None)
             found = True
         except:
@@ -19,17 +18,14 @@ def remove_coefs(clf):
 
 def remove_coefs_from_results(results):
     found = False
-    #for attr in ['param_clf', 'param_classifier', 'param_vectorizer', 'param_preprocessing']:
-    for attr in results.keys():
-        #if attr not in results: continue
-        param_clf = results[attr]
-        if np.ma.isMaskedArray(param_clf):
-            param_clf = np.ma.asarray(param_clf)
+    for attr, val in results.items():
+        if np.ma.isMaskedArray(val):
+            val = np.ma.asarray(val)
 
-        if not isinstance(param_clf, list) and not isinstance(param_clf, (np.ndarray, np.generic)) and not np.ma.isMaskedArray(param_clf):
-            param_clf = [param_clf]
+        if not isinstance(val, list) and not isinstance(val, (np.ndarray, np.generic)) and not np.ma.isMaskedArray(val):
+            val = [val]
 
-        for clf in param_clf:
+        for clf in val:
             found |= remove_coefs(clf)
 
     return found
