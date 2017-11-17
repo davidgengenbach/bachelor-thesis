4077b21f19e25c018f48c2d7f6310305b845efab
David Gengenbach <davidgengenbach@gmx.de>
2017-11-17 13:15:08 +0100

    Fix

--- code/utils/remove_coefs_from_results.py
+++ code/utils/remove_coefs_from_results.py
@@ -1,8 +1,9 @@
 import numpy as np
+import sklearn
 
 def remove_coefs(clf):
     found = False
-    for x in ['coef_', 'class_log_prior_', 'intercept_', 'feature_log_prob_', 'class_count_', 'feature_count_', 'vocabulary_', 'idf_', 'stop_words_', '_tfidf']:
+    for x in ['coef_', 'class_log_prior_', 'intercept_', 'feature_log_prob_', 'class_count_', 'feature_count_', 'vocabulary_', 'idf_', 'stop_words_', '_tfidf', 'scale_', 'max_abs_']:
         try:
             setattr(clf, x, None)
             found = True
@@ -19,8 +20,10 @@ def remove_coefs_from_results(results):
 
         if not isinstance(val, list) and not isinstance(val, (np.ndarray, np.generic)) and not np.ma.isMaskedArray(val):
             val = [val]
-
         for clf in val:
             found |= remove_coefs(clf)
+            if isinstance(clf, dict):
+                for k, v in clf.items():
+                    found |= remove_coefs(v)
 
     return found
